232ef368674f496040f86c36b956f6472af45cc3
Safa Kaysir <safa.kaysir@gmail.com>
2025-01-10 12:04:03 +0000

    calculation

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+function converting() {
+    const value = parseFloat(document.getElementById('input-box').value);
+    const unit = document.getElementById('select-size').value;
+    const outputunit = document.getElementById('select-size-output').value;
+  
+  
+  const convertingby = {
+    bytes:1,
+    kilobytes: 1024,
+    megabytes: 1024 * 1024,
+    gigabytes: 1024 * 1024 * 1024,
+  };
